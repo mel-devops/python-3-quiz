6790b21a598e966fdb57b5c97e37cdc2ce36f91e
Melissa Schaefer <mschaefer@xpanxion.com>
2023-07-10 10:38:34 -0400

    update

--- name_checker.py
+++ name_checker.py
@@ -2,12 +2,17 @@ from ValidationException import ValidationException
 
 def validate_file(file_path):
     pass #TODO: Only add code inside this function.
+    with open(file_path, 'r') as file:
+        
+        for line in file:
+            first_name = line.split()[0]
+            if any(char.isdigit() for char in first_name):
+                raise ValidationException(f"Invalid first name: {first_name}")
 
-
-def test():
+def name_checker():
     try:
-        validate_file("users.txt")
-    except ValidationException as ve:
-        print(ve)
+        validate_file('python-3-quiz/users.txt')
+    except ValidationException as validation:
+        print(validation)
 
-test()
+name_checker()
